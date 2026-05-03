f9481d6dfe289c6f28595cd6a1e3aa19c5bd6194
Junior <juniorn06@gmail.com>
2026-05-03 02:37:48 -0300

    feat(animal): adiciona campos peso, observacoes e formatação de data
    Implementa os atributos 'peso' e 'observacoes' na classe Animal. Adiciona também o método 'getDataFormatada' para o campo dataNascimento para facilitar a exibição no sistema.

--- src/main/java/com/jr/petland/entities/Animal.java
+++ src/main/java/com/jr/petland/entities/Animal.java
@@ -3,12 +3,18 @@ package com.jr.petland.entities;
 import com.jr.petland.enums.Sexo;
 import com.jr.petland.enums.TipoAnimal;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
 
 @Entity
 @NoArgsConstructor
@@ -16,29 +22,54 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Table(name = "animal")
-public class Animal {
+public class  Animal {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "TIPO_ANIMAL")
+    @NotNull
     private TipoAnimal tipoAnimal;
 
     @Column(name = "NOME")
+    @NotBlank(message = "O nome é obrigatório")
     private String nome;
 
     @Enumerated(EnumType.STRING)
     private Sexo sexo;
 
+    @PastOrPresent(message = "A data de nascimento não pode ser futura")
     @Column(name = "DATA_NASCIMENTO")
     private LocalDate dataNascimento;
 
     @Column(name = "RACA")
     private String raca;
 
+    @Column(name = "peso")
+    private Double peso;
+
+    @Column(name = "observacoes")
+    private String observacoes;
+
+    @CreationTimestamp
+    @Column(name = "data_cadastro", nullable = false, updatable = false)
+    private LocalDateTime dataCadastro;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cliente_id")
-    private Cliente cliente;
+    @NotNull(message = "O animal deve ter um dono")
+    private Cliente dono;
 
+    @Transient
+    public String getIdadeFormatada(){
+        if (this.dataNascimento == null){
+            return "Data não cadastrada!";
+        }
+
+        LocalDate hoje = LocalDate.now();
+        Period periodo = Period.between(this.dataNascimento, hoje);
+
+        return periodo.getYears() + " anos e " + periodo.getMonths() + "meses";
+    }
 }
